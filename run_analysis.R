61aee4405cbd207e7d23d38a55a64d2bce75f1d5
pbl64k <pbl64k@gmail.com>
2014-04-27 19:47:04 +0300

    Done with the script.

--- run_analysis.R
+++ run_analysis.R
@@ -7,6 +7,7 @@ data.file <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI
 local.data.file <- './original-dataset.zip'
 local.data.dir <- './UCI HAR Dataset'
 tidy.data.file <- './tidy-UCI-HAR-dataset.csv'
+tidy.avgs.data.file <- './tidy-UCI-HAR-avgs-dataset.csv'
 
 # Make sure the original data file is in the working directry, downloading
 # it if needed (and allowed)
@@ -77,8 +78,18 @@ X <- X[, grep('mean|std', feats$name)]
 # Convert activity labels to meaningful names
 y$activity <- acts[y$activity,]$name
 
-# Marge partial data sets together
+# Merge partial data sets together
 tidy.data.set <- cbind(subject, y, X)
 
 # Dump the data set
 write.csv(tidy.data.set, tidy.data.file)
+
+# Compute the averages grouped by subject and activity
+tidy.avgs.data.set <- aggregate(tidy.data.set[, 3:dim(tidy.data.set)[2]],
+                                list(tidy.data.set$subject,
+                                     tidy.data.set$activity),
+                                mean)
+names(tidy.avgs.data.set)[1:2] <- c('subject', 'activity')
+
+# Dump the second data set
+write.csv(tidy.avgs.data.set, tidy.avgs.data.file)
